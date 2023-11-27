15bc6b83aa353df4a464581565dbcbf2ff037047
hyunhomon <hyunhomon@gmail.com>
2023-11-28 04:53:21 +0900

    feat: 발표용 개발

--- SuperDrug/Source/Scene/GameScene.c
+++ SuperDrug/Source/Scene/GameScene.c
@@ -2,14 +2,245 @@
 #include "../../Header/Scene/GameScene.h"
 
 typedef struct {
+	double x, y, speed, shootTimer, shootDelay;
+	int w, h, curHp, maxHp;
+	bool idle, crouch, dir;
+
+	SDL_Texture* img;
+
+	Animation anim_idle;
+	Animation anim_stand_move_left;
+	Animation anim_stand_move_right;
+	Animation anim_stand_shoot;
+	Animation anim_crouch_move;
+	Animation anim_crouch_shoot;
+
+} Player;
+
+typedef struct {
+	double x, y, speed;
+	int w, h;
+	bool dir;
+
+	SDL_Texture* img;
+
+	Animation anim_move;
+
+} Goomba;
+
+typedef struct {
+	double x, y, speed;
+	int w, h;
+	bool dir;
+
+	SDL_Texture* img;
+
+} Mushroom;
+
+typedef struct {
+	double x, y, speed;
+	int w, h;
+	bool dir;
+
+	SDL_Texture* img_left;
+	SDL_Texture* img_right;
+
+	Mix_Chunk* sound;
+
+} Bullet;
+
+typedef struct {
 	int status;
+
+	SDL_Texture* img_bg;
+	SDL_Texture* img_player;
+	SDL_Texture* img_goomba;
+	SDL_Texture* img_mushroom;
+	SDL_Texture* img_bullet_left;
+	SDL_Texture* img_bullet_right;
+
+	Mix_Music* bgm;
+
+	Player player;
+	Goomba goombaBase;
+	Mushroom mushroomBase;
+	Bullet bulletBase;
+
+	List playerAnimList;
+	List goombaList;
+	List bulletList;
+
 } GameScene;
 
 GameScene gameScene;
 
+void Player_Init()
+{
+	Player* p = &gameScene.player;
+
+	p->x = 590.0;
+	p->y = 585.0;
+
+	p->img = gameScene.img_player;
+	getImageSize(p->img, &p->w, &p->h);
+
+	p->idle = true;
+	p->dir = true;
+
+	p->speed = 120.0;
+
+	p->curHp = 5;
+	p->maxHp = 5;
+
+	p->shootTimer = 0.0;
+	p->shootDelay = 0.5;
+}
+
+void Player_Update(double dt)
+{
+	Player* p = &gameScene.player;
+
+	int posX = 0, posY = 0;
+
+	if (getKeyState(SDL_SCANCODE_LEFT) == KEY_HOLD) {
+		Animation_Update(&p->anim_stand_move_left, dt);
+		p->img = getCurrentFrame(&p->anim_stand_move_left);
+
+		posX -= 1;
+		p->dir = false;
+		p->idle = false;
+	}
+	else if (getKeyState(SDL_SCANCODE_RIGHT) == KEY_HOLD) {
+		Animation_Update(&p->anim_stand_move_right, dt);
+		p->img = getCurrentFrame(&p->anim_stand_move_right);
+
+		posX += 1;
+		p->dir = true;
+		p->idle = false;
+	}
+	else {
+		p->idle = true;
+	}
+
+	p->x += posX * p->speed * dt;
+	p->y += posY * p->speed * dt;
+
+	if (p->x < 0.0) {
+		p->x = 0.0;
+	}
+	if (p->x > SCREEN_WIDTH - p->w) {
+		p->x = SCREEN_WIDTH - p->w;
+	}
+	if (p->y < 0.0) {
+		p->y = 0.0;
+	}
+	if (p->y > SCREEN_HEIGHT - p->h) {
+		p->y = SCREEN_HEIGHT - p->h;
+	}
+
+	p->shootTimer += dt;
+	if (getKeyState(SDL_SCANCODE_S) == KEY_HOLD) {
+		if (p->shootTimer >= p->shootDelay) {
+			
+
+			Bullet* bullet = (Bullet*)malloc(sizeof(Bullet));
+			memcpy(bullet, &gameScene.bulletBase, sizeof(Bullet));
+
+			if (p->dir) {
+				bullet->x = p->x + p->w;
+			}
+			else {
+				bullet->x = p->x - p->w;
+			}
+
+			bullet->y = p->y + p->h / 2 - 20;
+
+			bullet->dir = p->dir;
+
+			List_Push(&gameScene.bulletList, bullet);
+			
+			p->shootTimer = 0;
+		}
+	}
+
+	Node* bulletNode = gameScene.bulletList.head;
+
+	while (bulletNode != NULL) {
+		Bullet* bullet = (Bullet*)bulletNode->val;
+
+		if (bullet->dir) {
+			bullet->x += bullet->speed * dt;
+		}
+		else {
+			bullet->x -= bullet->speed * dt;
+		}
+
+		if (bullet->x > SCREEN_WIDTH || bullet->x < 0) {
+			bulletNode = bulletNode->next;
+			List_Remove(&gameScene.bulletList, bullet);
+
+			free(bullet);
+
+			continue;
+		}
+
+		bulletNode = bulletNode->next;
+	}
+}
+
 void GameScene_Init()
 {
 	SDL_Renderer* renderer = Get_Renderer();
+
+	gameScene.img_bg = Image_Load(renderer, "./Asset/Image/Background/Background.png");
+	gameScene.img_player = Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Right/0.png");
+	gameScene.img_bullet_left = Image_Load(renderer, "./Asset/Image/Mario/Object/BulletLeft.png");
+	gameScene.img_bullet_right = Image_Load(renderer, "./Asset/Image/Mario/Object/BulletRight.png");
+
+	Player_Init();
+
+	gameScene.bulletBase.img_left = gameScene.img_bullet_left;
+	gameScene.bulletBase.img_right = gameScene.img_bullet_right;
+
+	getImageSize(gameScene.img_bullet_left, &gameScene.bulletBase.w, &gameScene.bulletBase.h);
+	gameScene.bulletBase.speed = 700.0;
+
+	/*gameScene.bgm = Mix_LoadWAV("./Asset/Sound/Mario/BGM/overworld.wav");
+	gameScene.bulletBase.sound = Mix_LoadWAV("./Asset/Sound/Mario/blockhit.wav");*/
+
+	List_Init(&gameScene.playerAnimList);
+	List_Init(&gameScene.bulletList);
+
+	Animation_Init(&gameScene.player.anim_stand_move_left, 10, ANIM_LOOP_ON);
+	Animation_Init(&gameScene.player.anim_stand_move_right, 10, ANIM_LOOP_ON);
+
+	appendTexture(&gameScene.player.anim_stand_move_left, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Left/0.png"));
+	appendTexture(&gameScene.player.anim_stand_move_left, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Left/1.png"));
+	appendTexture(&gameScene.player.anim_stand_move_left, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Left/2.png"));
+	appendTexture(&gameScene.player.anim_stand_move_left, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Left/3.png"));
+	appendTexture(&gameScene.player.anim_stand_move_left, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Left/4.png"));
+	appendTexture(&gameScene.player.anim_stand_move_left, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Left/5.png"));
+	appendTexture(&gameScene.player.anim_stand_move_left, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Left/6.png"));
+	appendTexture(&gameScene.player.anim_stand_move_left, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Left/7.png"));
+	appendTexture(&gameScene.player.anim_stand_move_left, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Left/8.png"));
+	appendTexture(&gameScene.player.anim_stand_move_left, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Left/9.png"));
+	appendTexture(&gameScene.player.anim_stand_move_left, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Left/10.png"));
+	appendTexture(&gameScene.player.anim_stand_move_left, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Left/11.png"));
+
+	appendTexture(&gameScene.player.anim_stand_move_right, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Right/0.png"));
+	appendTexture(&gameScene.player.anim_stand_move_right, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Right/1.png"));
+	appendTexture(&gameScene.player.anim_stand_move_right, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Right/2.png"));
+	appendTexture(&gameScene.player.anim_stand_move_right, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Right/3.png"));
+	appendTexture(&gameScene.player.anim_stand_move_right, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Right/4.png"));
+	appendTexture(&gameScene.player.anim_stand_move_right, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Right/5.png"));
+	appendTexture(&gameScene.player.anim_stand_move_right, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Right/6.png"));
+	appendTexture(&gameScene.player.anim_stand_move_right, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Right/7.png"));
+	appendTexture(&gameScene.player.anim_stand_move_right, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Right/8.png"));
+	appendTexture(&gameScene.player.anim_stand_move_right, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Right/9.png"));
+	appendTexture(&gameScene.player.anim_stand_move_right, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Right/10.png"));
+	appendTexture(&gameScene.player.anim_stand_move_right, Image_Load(renderer, "./Asset/Image/MetalSlug/PlayerWalking/Right/11.png"));
+
+	/*Mix_PlayMusic(gameScene.bgm, 1);*/
 }
 
 void GameScene_Event()
@@ -30,6 +261,8 @@ void GameScene_Update()
 
 	int mouseX = 0, mouseY = 0;
 	getCurrentMousePos(&mouseX, &mouseY);
+
+	Player_Update(dt);
 }
 
 void GameScene_Render()
@@ -39,11 +272,40 @@ void GameScene_Render()
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
 	SDL_RenderClear(renderer);
 
+	Image_Draw(renderer, 0, 0, gameScene.img_bg);
+	Image_Draw(renderer, gameScene.player.x, gameScene.player.y, gameScene.player.img);
+	
+	Node* bulletNode = gameScene.bulletList.head;
+	while (bulletNode != NULL) {
+		Bullet* bullet = (Bullet*)bulletNode->val;
+
+		if (bullet->dir) {
+			Image_Draw(renderer, bullet->x, bullet->y, bullet->img_right);
+		}
+		else {
+			Image_Draw(renderer, bullet->x, bullet->y, bullet->img_left);
+		}
+		
+		bulletNode = bulletNode->next;
+	}
+
 	SDL_RenderPresent(renderer);
 }
 
 void GameScene_Release()
 {
+	SDL_DestroyTexture(gameScene.img_bg);
+	SDL_DestroyTexture(gameScene.player.img);
+	SDL_DestroyTexture(gameScene.img_bullet_left);
+	SDL_DestroyTexture(gameScene.img_bullet_right);
+
+	List_Release(&gameScene.playerAnimList);
+	List_Release(&gameScene.bulletList);
+
+	Animation_Release_With_Texture(&gameScene.player.anim_stand_move_left);
+	Animation_Release_With_Texture(&gameScene.player.anim_stand_move_right);
+
+	/*Mix_FreeMusic(gameScene.bgm);*/
 }
 
 int GameScene_Loop()
--- SuperDrug/Source/Scene/MainScene.c
+++ SuperDrug/Source/Scene/MainScene.c
@@ -4,9 +4,12 @@
 typedef struct {
 	int status;
 
+	SDL_Texture* img_bg;
 	SDL_Texture* img_logo;
+	SDL_Texture* img_start;
 
 	SDL_Rect rect_logo;
+	SDL_Rect rect_start;
 
 } MainScene;
 
@@ -15,6 +18,18 @@ MainScene mainScene;
 void MainScene_Init()
 {
 	SDL_Renderer* renderer = Get_Renderer();
+
+	mainScene.img_bg = Image_Load(renderer, "./Asset/Image/Background/Background.png");
+	mainScene.img_logo = Image_Load(renderer, "./Asset/Image/Logo/Logo.png");
+	mainScene.img_start = Image_Load(renderer, "./Asset/Image/Background/start.png");
+
+	getImageSize(mainScene.img_logo, &mainScene.rect_logo.w, &mainScene.rect_logo.h);
+	mainScene.rect_logo.x = 540.0;
+	mainScene.rect_logo.y = 100.0;
+
+	getImageSize(mainScene.img_start, &mainScene.rect_start.w, &mainScene.rect_start.h);
+	mainScene.rect_start.x = 411;
+	mainScene.rect_start.y = 531;
 }
 
 void MainScene_Event()
@@ -35,6 +50,26 @@ void MainScene_Update()
 
 	int mouseX = 0, mouseY = 0;
 	getCurrentMousePos(&mouseX, &mouseY);
+
+	if (getButtonState(SDL_BUTTON_LEFT) == KEY_DOWN) {
+		if (Collision_Check_With_Point(mainScene.rect_logo, mouseX, mouseY)) {
+			printf("차수현의 현호를 찾아서 발표가 이후에 이어질 예정입니다.");
+			mainScene.status = EXIT_GAME;
+		}
+		if (Collision_Check_With_Point(mainScene.rect_start, mouseX, mouseY)) {
+			printf("GAME START!\n");
+			mainScene.status = GAME_SCENE;
+		}
+	}
+
+	if (getKeyState(SDL_SCANCODE_SPACE) == KEY_DOWN) {
+		printf("GAME START!\n");
+		mainScene.status = GAME_SCENE;
+	}
+	if (getKeyState(SDL_SCANCODE_ESCAPE) == KEY_DOWN) {
+		printf("EXIT!\n");
+		mainScene.status = EXIT_GAME;
+	}
 }
 
 void MainScene_Render()
@@ -44,11 +79,18 @@ void MainScene_Render()
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
 	SDL_RenderClear(renderer);
 
+	Image_Draw(renderer, 0, 0, mainScene.img_bg);
+	Image_Draw(renderer, mainScene.rect_logo.x, mainScene.rect_logo.y, mainScene.img_logo);
+	Image_Draw(renderer, mainScene.rect_start.x, mainScene.rect_start.y, mainScene.img_start);
+
 	SDL_RenderPresent(renderer);
 }
 
 void MainScene_Release()
 {
+	SDL_DestroyTexture(mainScene.img_bg);
+	SDL_DestroyTexture(mainScene.img_logo);
+	SDL_DestroyTexture(mainScene.img_start);
 }
 
 int MainScene_Loop()
